81fdd5ecbf346d30bacd0f4804f5700fc51d73c5
Joel Ndiba Mwaura <90656444+xjomwnd@users.noreply.github.com>
2024-01-14 21:49:47 +0300

    COMMIT

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
-//This is the application server
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
- 
-const express = require("express")
-
-const app = express()
-
-/* ******************************************
- * Server host name and port
-
- * ***************************************** */
-const HOST = 'localhost'
-const PORT = 3000
-
-/* ***********************
-* Log statement to confirm server operation
-* *********************** */
-app.listen(PORT, () => {
-console.log(`trial app listening on ${HOST}:${PORT}`)
-})
 
 // Other routes may be defined here...
 
